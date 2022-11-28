f897d5747fdbdf912630ebbf5985b29f7233951e
Kyle Weidner <49170524+kweidner14@users.noreply.github.com>
2022-11-27 23:23:43 -0700

    Add ICO Smart Contract

--- erc20-token.sol
+++ erc20-token.sol
@@ -20,7 +20,7 @@ interface ERC20Interface {
 }
  
  
-contract Cryptos is ERC20Interface{
+contract Mycelium is ERC20Interface {
     string public name = "Mycelium";
     string public symbol = "MYC";
     uint public decimals = 0; //18 is very common
@@ -34,19 +34,19 @@ contract Cryptos is ERC20Interface{
     // allowed[0x111][0x222] = 100;
     
     
-    constructor(){
+    constructor() {
         totalSupply = 1000000;
         founder = msg.sender;
         balances[founder] = totalSupply;
     }
     
     
-    function balanceOf(address tokenOwner) public view override returns (uint balance){
+    function balanceOf(address tokenOwner) public view override returns (uint balance) {
         return balances[tokenOwner];
     }
     
     
-    function transfer(address to, uint tokens) public override returns(bool success){
+    function transfer(address to, uint tokens) public override returns(bool success) {
         require(balances[msg.sender] >= tokens);
         
         balances[to] += tokens;
@@ -57,12 +57,12 @@ contract Cryptos is ERC20Interface{
     }
     
     
-    function allowance(address tokenOwner, address spender) view public override returns(uint){
+    function allowance(address tokenOwner, address spender) view public override returns(uint) {
         return allowed[tokenOwner][spender];
     }
     
     
-    function approve(address spender, uint tokens) public override returns (bool success){
+    function approve(address spender, uint tokens) public override returns (bool success) {
         require(balances[msg.sender] >= tokens);
         require(tokens > 0);
         
@@ -73,7 +73,7 @@ contract Cryptos is ERC20Interface{
     }
     
     
-    function transferFrom(address from, address to, uint tokens) public override returns (bool success){
+    function transferFrom(address from, address to, uint tokens) public override returns (bool success) {
          require(allowed[from][msg.sender] >= tokens);
          require(balances[from] >= tokens);
          
@@ -85,4 +85,84 @@ contract Cryptos is ERC20Interface{
          
          return true;
      }
+}
+
+contract MyceliumICO is Mycelium {
+    address public admin;
+    address payable public deposit;
+    uint tokenPrice = 0.001 ether; // 1 ETH = 1000 MYC
+    uint public hardCap = 300 ether;
+    uint public raisedAmount;
+    uint public saleStart = block.timestamp;
+    uint public saleEnd = block.timestamp + 604800; //ICO ends after 1 week
+    uint public tokenTradeStart = saleEnd + 604800; // Tokens are sellable 1 week after sale ends
+    uint public maxInvestment = 5 ether;
+    uint public minInvestment = .1 ether;
+
+    enum State{beforeStart, running, afterEnd, halted}
+    State public ICOState;
+
+    constructor(address payable _deposit) {
+        deposit = _deposit;
+        admin = msg.sender;
+        ICOState = State.beforeStart;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin);
+        _;
+    }
+
+    function halt() public onlyAdmin {
+        ICOState = State.halted;
+    }
+
+    function resume() public onlyAdmin {
+        ICOState = State.running;
+    }
+
+    function changeDepositAddress(address payable newDeposit) public onlyAdmin {
+        deposit = newDeposit;
+    }
+
+    function getCurrentState() public view returns(State) {
+        if(ICOState == State.halted) {
+            return State.halted;
+        }
+        else if (block.timestamp < saleStart) {
+            return State.beforeStart;
+        }
+        else if(block.timestamp >= saleStart && block.timestamp <= saleEnd) {
+            return State.running;
+        }
+        else {
+            return State.afterEnd;
+        }
+    }
+
+    event Invest(address investor, uint value, uint tokens);
+
+    function invest() payable public returns(bool) {
+        ICOState = getCurrentState();
+        require(ICOState == State.running);
+        require(msg.value >= minInvestment && msg.value <= maxInvestment);
+        raisedAmount += msg.value;
+        require(raisedAmount <= hardCap);
+
+        uint tokens = msg.value / tokenPrice;
+
+        balances[msg.sender] += tokens;
+        balances[founder] -= tokens;
+
+        deposit.transfer(msg.value);
+
+        emit Invest(msg.sender, msg.value, tokens);
+        return true;
+    }
+
+    receive() payable external {
+        invest();
+    }
+
+
 }
